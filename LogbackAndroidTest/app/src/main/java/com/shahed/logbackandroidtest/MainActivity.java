f75f3a54bc160c0fb813cd9613f9d279494dde0c
Shahed Iqbal <shishir.cuet@gmail.com>
2017-04-29 15:04:01 +0600

    Initial commit

---
+++ LogbackAndroidTest/app/src/main/java/com/shahed/logbackandroidtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shahed.logbackandroidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
